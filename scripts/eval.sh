92c7710df5522dd204caf90c7bf25addf0c4b286
Oldrich Tristan Florian <oldrich.florian@gmail.com>
2021-03-24 22:55:04 +0100

    ok, changing the names back :D

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,8 +2,8 @@
 
 out_dir="output"
 
-for ref_file in ../pa193_dataset/dataset/*.json; do
+for ref_file in pa193_dataset/dataset/*.json; do
     out_file="$out_dir/$(basename $ref_file)"
     echo evaluating "$out_file"...
-    python3 ../pa193_dataset/output_compare.py "$out_file" "$ref_file"
+    python3 pa193_dataset/output_compare.py "$out_file" "$ref_file"
 done
